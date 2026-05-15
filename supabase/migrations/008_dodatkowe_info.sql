fed81e5c3832cd1d8f4c650be7801e065d4d668d
muststopx-blip <muststopx@gmail.com>
2026-05-15 10:33:05 +0200

    Delete supabase directory

--- supabase/migrations/008_dodatkowe_info.sql
+++
@@ -1,3 +0,0 @@
--- Dodaj pole dodatkowe_info do załadunków i rozładunków
-ALTER TABLE zaladunki ADD COLUMN IF NOT EXISTS dodatkowe_info TEXT;
-ALTER TABLE rozladunki ADD COLUMN IF NOT EXISTS dodatkowe_info TEXT;
